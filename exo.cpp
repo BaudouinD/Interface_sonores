e2f0e67692d8a3225ea2e08d3feb7d844e48722a
DOULIERY Baudouin <Baudouin.Douliery@bordeaux-inp.fr>
2021-02-24 11:17:25 +0100

    synthe

---
+++ exo.cpp
@@ -0,0 +1,14 @@
+#define RTMIDI17_HEADER_ONLY 1
+#include <../rtmidi17/reader.hpp>
+#include <iostream>
+#include <fstream>
+
+int main(void)
+{
+std::vector<uint8_t> bytes;
+std::ifstream file{"/chemin/vers/fichier.mid", std::ios::binary};
+bytes.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+rtmidi::reader reader;
+reader.parse(bytes);
+// reader contient maintenant les informations contenues dans le fichier midi.
+}
